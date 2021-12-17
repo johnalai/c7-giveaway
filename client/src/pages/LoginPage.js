b34255f73fcfe78369a14f1a6a701021cc0fbb84
derek-birtwistle <derekbirtwistle@hotmail.com>
2021-12-16 18:16:20 -0700

    login navigate

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,58 +1,87 @@
-import { useState,useContext } from 'react'
-import { useNavigate } from "react-router-dom"
-import { UserContext } from '../components/UserContext'
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../components/UserContext";
 
 const LoginPage = () => {
-    const {currentUser,setCurrentUser}=useContext(UserContext)
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loginError, setLoginError] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function tryLogin() {
-        async function postLogin() {
-            const loginInfo = {
-                username: username, 
-                password: password
-            }
-            let loginResult = await fetch('/auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(loginInfo)
-            })
-            if (loginResult.ok) {
-                alert('Hello: '+username)
-                setLoginError('')
-                navigate('/user/:id')
-            }
-            else {
-                setLoginError('Login failed!')
-            }
-        }
-        postLogin()
-        setCurrentUser(username)
-      
-      
+  useEffect(() => {
+   
+    alert("Hello: " + currentUser?.username);
+    if (currentUser?.username) {
+      navigate("/user/" + currentUser.id);
     }
-    
-    return (
-        <div className='container'>
-            <div className="row">
-                <label>Username</label>
-                <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-            </div>
-            <div className="row">
-                <label>Password</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <button className="btn btn-primary" onClick={tryLogin}>Login</button>
-            { loginError !== '' && <div className='alert alert-danger'>{loginError}</div> }
-        </div>
-    )
-}
+  }, [currentUser]);
 
-export default LoginPage
+  function tryLogin() {
+    async function postLogin() {
+      const loginInfo = {
+        username: username,
+        password: password,
+      };
+      let loginResult = await fetch("/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+
+      if (loginResult.ok) {
+        async function fetchUser() {
+          let fetchResult = await fetch("/auth/loggedInUser");
+          let fetchedUser = await fetchResult.json();
+          setCurrentUser((curr) => {
+            let newObj = { ...curr };
+            newObj = fetchedUser;
+            return newObj;
+          });
+        }
+
+        function loginOk() {
+          setLoginError("");
+        }
+
+        function login() {
+          fetchUser().then(loginOk()).catch();
+        }
+        login();
+      } else {
+        setLoginError("Login failed!");
+      }
+    }
+
+    postLogin();
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <label>Username</label>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div className="row">
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary" onClick={tryLogin}>
+        Login
+      </button>
+      {loginError !== "" && (
+        <div className="alert alert-danger">{loginError}</div>
+      )}
+    </div>
+  );
+};
+
+export default LoginPage;
